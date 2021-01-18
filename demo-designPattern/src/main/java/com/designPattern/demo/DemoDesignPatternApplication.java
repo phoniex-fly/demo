5b9cbdf8cec581b20e1be3a767061e91373b5f20
phoniex-fly <shixiaoyan@sinosoft.com.cn>
2021-01-18 11:29:12 +0800

    designPattern init

---
+++ demo-designPattern/src/main/java/com/designPattern/demo/DemoDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.designPattern.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDesignPatternApplication.class, args);
+	}
+
+}
