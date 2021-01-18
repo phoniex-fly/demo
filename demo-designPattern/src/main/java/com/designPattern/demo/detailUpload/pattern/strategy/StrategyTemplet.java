c160f3edffc635b9adb50b60da753ae47739a02a
phoniex-fly <shixiaoyan@sinosoft.com.cn>
2021-01-18 17:10:47 +0800

    遗留问题：多个策略模式是否可抽象模板父类

---
+++ demo-designPattern/src/main/java/com/designPattern/demo/detailUpload/pattern/strategy/StrategyTemplet.java
@@ -0,0 +1,13 @@
+package com.designPattern.demo.detailUpload.pattern.strategy;
+
+/**
+ *
+ * @ClassName: StrategyTemplet
+ * @Description:TODO(策略模板)
+ * @author: Shixiaoyan
+ * @date: 2021年1月18日 下午4:49:26
+ * @Copyright:
+ */
+public class StrategyTemplet {
+
+}
