c30dd2734a79510fc7e1d51dce3328d23ca50cd4
phoniex-fly <shixiaoyan@sinosoft.com.cn>
2021-01-18 17:41:25 +0800

    ddd init

---
+++ demo-DDD/src/main/java/com/ddd/demo/DemoDddApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDddApplication.class, args);
+	}
+
+}
