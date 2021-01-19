392c2a01918ca2f7bcbb10e542293cc988874468
phoniex-fly <shixiaoyan@sinosoft.com.cn>
2021-01-19 10:25:30 +0800

    模板模式应用：系统所有导出功能统一导出模板

---
+++ demo-designPattern/src/main/java/com/designPattern/demo/export/controller/ContractController.java
@@ -0,0 +1,28 @@
+package com.designPattern.demo.export.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.designPattern.demo.export.service.ContractService;
+
+/**
+ * 
+ * @ClassName: ContractController
+ * @Description:TODO(描述这个类的作用)
+ * @author: Shixiaoyan
+ * @date: 2021年1月19日 上午10:24:00
+ * @Copyright:
+ */
+@Controller
+@RequestMapping("contract")
+public class ContractController {
+
+	@Autowired
+	private ContractService contractService;
+
+	public void export() {
+		contractService.exportContract();
+	}
+
+}
