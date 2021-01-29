90cb67910da2d185e465b777035e0281113e5b20
phoniex-fly <shixiaoyan@sinosoft.com.cn>
2021-01-29 16:41:56 +0800

    readme说明文件编辑

--- demo-DDD/src/main/java/com/ddd/demo/DemoDDDApplication.java
+++ demo-DDD/src/main/java/com/ddd/demo/DemoDDDApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoDddApplication {
+public class DemoDDDApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoDddApplication.class, args);
+		SpringApplication.run(DemoDDDApplication.class, args);
 	}
 
 }
--- demo-DDD/src/test/java/com/ddd/demo/DemoDDDApplicationTests.java
+++ demo-DDD/src/test/java/com/ddd/demo/DemoDDDApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoDddApplicationTests {
+class DemoDDDApplicationTests {
 
 	@Test
 	void contextLoads() {
